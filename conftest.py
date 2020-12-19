778d2e136960cd123381bdb9fe5f29ea7d26704f
howardwu <howardwu@crisisgord.com>
2020-12-19 16:43:42 +0800

    添加注释

--- conftest.py
+++ conftest.py
@@ -12,19 +12,24 @@ cf.read(f'{os_path}/config.ini')
 
 def get_cookies():
     d = Driver()
+    # 启动debug的浏览器
     d.debug_start()
     d.debug_driver.get(cf.get('Test', 'main_url'))
     f = open(f'{os_path}/pages/cookies.yaml', 'w', encoding='utf-8')
+    # 获取cookie并直接存储到yaml文件中
     yaml.dump(Driver().debug_driver.get_cookies(), f)
 
 
 @pytest.fixture(scope='session', autouse=True)
 def web_start():
+    # cookie复用
     get_cookies()
     d = Driver()
     d.start()
     d.driver.get(cf.get('Test', 'login_url'))
+    # 导入复用的cookie
     [d.driver.add_cookie(cookie) for cookie in yaml.safe_load(open(f"{os_path}/pages/cookies.yaml", encoding='utf-8'))]
+    # 启动浏览器
     d.driver.get(cf.get('Test', 'main_url'))
     yield
     time.sleep(5)
--- testcase/test_wechat_contacts.py
+++ testcase/test_wechat_contacts.py
@@ -7,6 +7,14 @@ from pages.main_page import MainPage
 os_path = os.path.dirname(os.path.abspath(__file__))
 test_data = yaml.safe_load(open(f'{os_path}/contact_data.yaml', encoding='utf-8'))
 
+'''
+注释:
+1. 用例使用PO结构.
+2. driver启动逻辑在conftest.py中进行了定义. 启动前需要确保debug的chrome浏览器已开启, 并已成功登录了企业微信, 从而执行时可以直接获取复用cookie.
+3. 元素定位业务逻辑都在对应的pages的类中, testcase中直接引入方法并调用.
+4. 数据验证时使用整行数据信息进行验证, 确保单个人员添加的整体信息都是正确的.
+'''
+
 
 @allure.feature("联系人功能测试")
 @pytest.mark.usefixtures("web_start")
@@ -14,7 +22,7 @@ class TestContacts:
 
     @allure.title("联系人添加")
     @pytest.mark.contacts
-    @pytest.mark.parametrize("user_info", test_data, ids=[name[2] for name in test_data])
+    @pytest.mark.parametrize("user_info", test_data, ids=[name[2] for name in test_data])  # 取account作为ids
     def test_add_contacts(self, user_info):
         name, alias, account, phone, email, job, department = user_info
         with allure.step("测试创建账号"):
